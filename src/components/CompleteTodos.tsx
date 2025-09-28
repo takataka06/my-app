2799d68bd85150c9faf9d14eb3b39ffc2a1bd49d
takataka06 <reyreyrey0110@gmail.com>
2025-09-28 22:39:20 +0900

    Add several components

---
+++ src/components/CompleteTodos.tsx
@@ -0,0 +1,22 @@
+import type {Todo} from "../App.tsx";
+
+type Props = {
+  TodoLists: Todo[];
+}
+
+export default function CompleteTodos({TodoLists}:Props) {
+  return (
+    <div>
+        <h2 className='font-bold text-lg'>完了</h2>
+        <ul>
+          {TodoLists.filter(todo => todo.completed).map(todo => (
+            <li key={todo.id} className="flex items-center">
+              <p>{todo.title}</p>
+              <button className="block bg-white text-amber-600 rounded-lg p-1 mt-3 ml-4 active:translate-y-1"
+              >戻す</button>
+            </li>
+          ))}
+        </ul>
+    </div>
+  )
+}
